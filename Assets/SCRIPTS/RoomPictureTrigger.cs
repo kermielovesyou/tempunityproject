da66e86e1e85864cbef528584e959ad03a11b934
Stephanie Henderson <kermielovesyou@gmail.com>
2016-11-02 12:58:55 -0700

    pictures working

---
+++ Assets/SCRIPTS/RoomPictureTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomPictureTrigger : MonoBehaviour {
+	GameManager gameManager;
+	[SerializeField] GameObject fishPicture;
+
+	// Use this for initialization
+	void Awake () {
+		gameManager = FindObjectOfType<GameManager> ();
+		fishPicture.GetComponent<Renderer> ().enabled = false;
+	}
+
+
+	void OnTriggerEnter (Collider coll) {
+		if (gameManager.SaidOkToFish) {
+			fishPicture.GetComponent<Renderer> ().enabled = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
